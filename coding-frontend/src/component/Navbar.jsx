37bf3b20feaf6d4ab76ff4b2ff71cbc14d4d78d5
kyb <kyb-0215@naver.com>
2024-07-13 22:31:35 +0900

    네브바 수정 및 npm install

--- coding-frontend/src/component/Navbar.jsx
+++ coding-frontend/src/component/Navbar.jsx
@@ -1,11 +1,17 @@
-import {Center, Flex} from "@chakra-ui/react";
+import {Box, Center, Flex, Image} from "@chakra-ui/react";
 import {useNavigate} from "react-router-dom";
 
 export function Navbar() {
     const navigate = useNavigate();
     return (
-        <Flex h={"50px"} cursor={"pointer"} backgroundColor={"#2AC1BC"}>
-            <Center onClick={()=>navigate("map")}>
+        <Flex h={"55px"} cursor={"pointer"} backgroundColor={"#2AC1BC"}>
+            <Box>
+                <Box cursor={"pointer"} w={"200px"} onClick={() => navigate("/")}>
+                    <Image src={"/img/pickUp_black.png"} />
+                </Box>
+            </Box>
+
+            <Center onClick={()=>navigate("restaurant")}>
                 지도
             </Center>
             <Center>
--- coding-frontend/src/restaurant/RestaurantMenuList.jsx
+++ coding-frontend/src/restaurant/RestaurantMenuList.jsx
@@ -1,9 +1,34 @@
-import {Box} from "@chakra-ui/react";
+import {Box, Image, Spinner, Text} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export function RestaurantMenuList() {
+    const { placeId } = useParams();
+    const [menuList, setMenuList] = useState(null);
+
+    useEffect(() => {
+            axios.get(`/api/menus/${placeId}`)
+                .then((res) => {
+                    console.log(res.data);
+                    setMenuList(res.data);
+                })
+
+    }, []);
+
+    if (menuList === null) {
+        return <Spinner />;
+    }
+
     return (
         <Box>
-            menu
+            {/*{menuList.map((menu, index) => (*/}
+            {/*    <Box key={index}>*/}
+            {/*        <Image src={menu.image} alt={menu.name} />*/}
+            {/*        <Text>{menu.name}</Text>*/}
+            {/*        <Text>{menu.price}</Text>*/}
+            {/*    </Box>*/}
+            {/*))}*/}
         </Box>
     );
 }
