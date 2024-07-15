519ca355007bb13587ac5a4cd1912e277fd28199
jeongyun <zoe1126@naver.com>
2024-07-15 17:43:32 +0900

    머지 컴플릭트 해결

--- coding-frontend/src/App.jsx
+++ coding-frontend/src/App.jsx
@@ -1,41 +1,36 @@
-import {ChakraProvider, MenuList} from "@chakra-ui/react";
-import React from 'react';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {Home} from "./Home.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./Home.jsx";
 import RestaurantMapView from "./restaurant/RestaurantMapView.jsx";
 import "./styles/fonts.css";
-import {theme} from "./component/theme.jsx";
-import {MainPage} from "./restaurant/MainPage.jsx";
-import {RestaurantMenuList} from "./restaurant/RestaurantMenuList.jsx";
-import MapView from "./Map/MapView.jsx";
+import { theme } from "./component/theme.jsx";
+import { MainPage } from "./restaurant/MainPage.jsx";
+import { RestaurantMenuList } from "./restaurant/RestaurantMenuList.jsx";
 import Login from "./User/Login.jsx";
-import {Signup} from "./User/Signup.jsx";
-
-
+import { Signup } from "./User/Signup.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home/>,
-        children:[
-
-                // user
-            {index:true, element:<MainPage/>},
-            {path:"signup", element: <Signup/>},
-            {path:"login", element:<Login/>},
-                // map
-            {path:"restaurant", element:<RestaurantMapView/>},
-            {path:"menu/:placeId", element:<RestaurantMenuList/>},
-
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      // user
+      { index: true, element: <MainPage /> },
+      { path: "signup", element: <Signup /> },
+      { path: "login", element: <Login /> },
+      // map
+      { path: "restaurant", element: <RestaurantMapView /> },
+      { path: "menu/:placeId", element: <RestaurantMenuList /> },
+    ],
+  },
+]);
 function App(props) {
-    return (
-        <ChakraProvider theme={theme}>
-            <RouterProvider router={router} />
-        </ChakraProvider>
-    );
+  return (
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
 }
 
 export default App;
