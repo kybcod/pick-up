fc92cbe35c1bf544e0fec226245dcba6d2bc2f6f
kyb <kyb-0215@naver.com>
2024-07-16 10:35:53 +0900

    장바구니, 메뉴 리스트

---
+++ coding-frontend/src/restaurant/MenuList.jsx
@@ -0,0 +1,22 @@
+import {Box, Button, Flex, Image, Text, VStack} from "@chakra-ui/react";
+
+export function MenuList({ menuList, cart, handleAdd, handleRemove }) {
+    return (
+        <VStack spacing={4} align="stretch">
+            {menuList.map((menu, index) => (
+                <Flex key={index} justifyContent="space-between" alignItems="center" p={4} borderWidth={1} borderRadius="lg">
+                    <Box>
+                        {menu.img && <Image src={menu.img} boxSize="100px"/>}
+                        <Text fontWeight="bold" mt={2}>{menu.menu}</Text>
+                        <Text>{menu.price}원</Text>
+                    </Box>
+                    <Flex alignItems="center">
+                        <Button onClick={() => handleAdd(menu)} colorScheme="teal" size="sm" mr={2}>+</Button>
+                        <Text>{cart[menu.menu]?.count || 0}</Text>
+                        <Button onClick={() => handleRemove(menu)} colorScheme="teal" size="sm" ml={2}>-</Button>
+                    </Flex>
+                </Flex>
+            ))}
+        </VStack>
+    );
+}
