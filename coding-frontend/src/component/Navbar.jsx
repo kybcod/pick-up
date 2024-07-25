5caf944b22e5ee76bf31795071352624d17a2d21
jeongyun <zoe1126@naver.com>
2024-07-25 10:48:05 +0900

    네브바 수정

--- coding-frontend/src/component/Navbar.jsx
+++ coding-frontend/src/component/Navbar.jsx
@@ -1,12 +1,10 @@
-import { Box, Center, Flex, Image, Spacer } from "@chakra-ui/react";
+import { Box, Flex, Image, Spacer } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
-import React, { useContext } from "react";
+import React from "react";
 import { DrawerExample } from "./DrawerExample.jsx";
-import { LoginContext } from "./LoginProvider.jsx";
 
 export function Navbar() {
   const navigate = useNavigate();
-  const account = useContext(LoginContext);
 
   return (
     <Flex
@@ -21,26 +19,6 @@ export function Navbar() {
         <Image src="/img/pickUp_black.png" alt="Logo" />
       </Box>
       <Spacer />
-      {account.isLoggedIn() || (
-        <Center>
-          <Center onClick={() => navigate("login")}>login</Center>
-          <Center ml={1} onClick={() => navigate("signup")}>
-            / sign-up
-          </Center>
-        </Center>
-      )}
-      {account.isLoggedIn() && (
-        <Center>
-          <Center
-            onClick={() => {
-              account.logout();
-              navigate("/");
-            }}
-          >
-            logout
-          </Center>
-        </Center>
-      )}
 
       <DrawerExample />
     </Flex>
